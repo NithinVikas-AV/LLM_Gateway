0d56a39c5531fcec78eabe373eb2efa2d3a43758
Nithin Vikas <nithinvikas8389@gmail.com>
2026-03-16 23:57:04 +0530

    Code for Alembic to detect tables.

---
+++ backend/app/db/base.py
@@ -0,0 +1,7 @@
+# imports all models for Alembic
+# Every model will inherit from this Base. Alembic needs it to detect your tables.
+
+from sqlalchemy.orm import DeclarativeBase
+
+class Beta(DeclarativeBase):
+    pass
