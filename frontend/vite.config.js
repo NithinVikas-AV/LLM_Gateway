10bb3c684a63fb9b083f63d85de0237043184a89
Nithin Vikas <nithinvikas8389@gmail.com>
2026-03-19 18:40:22 +0530

    frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,30 @@
+// For Normal runs
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import path from 'path'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+//   server: {
+//     proxy: {
+//       '/auth/google': 'http://localhost:8000',
+//       '/auth/callback': 'http://localhost:8000',
+//       '/keys': 'http://localhost:8000',
+//       '/gateway': 'http://localhost:8000',
+//       '/usage': 'http://localhost:8000',
+//       '/users': 'http://localhost:8000',
+//     }
+//   }
+// })
+
+// For Docker runs
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
@@ -5,18 +32,17 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+    alias: { "@": path.resolve(__dirname, "./src") },
   },
   server: {
+    host: '0.0.0.0',
+    port: 5173,
     proxy: {
-      '/auth/google': 'http://localhost:8000',
-      '/auth/callback': 'http://localhost:8000',
-      '/keys': 'http://localhost:8000',
-      '/gateway': 'http://localhost:8000',
-      '/usage': 'http://localhost:8000',
-      '/users': 'http://localhost:8000',
+      '/auth': 'http://backend:8000',
+      '/keys': 'http://backend:8000',
+      '/gateway': 'http://backend:8000',
+      '/usage': 'http://backend:8000',
+      '/users': 'http://backend:8000',
     }
   }
 })
