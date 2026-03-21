5391a4080b4af0c9210e265bba9c0c4cbf0f280e
Nithin Vikas <nithinvikas8389@gmail.com>
2026-03-21 22:49:08 +0530

    Universal Chat Code

--- Universal_Chat.py
+++ Universal_Chat.py
@@ -1,7 +1,7 @@
 import requests
 
 UNIVERSAL_KEY = "llmgw-gp6aSQ7bUH0dGnnF93JN940bprI7cLn_TlvmeuW0XEY"
-BASE_URL = "http://localhost:8000"
+BASE_URL = "https://llmgateway-production.up.railway.app"
 
 response = requests.post(
     f"{BASE_URL}/gateway/chat",
@@ -12,5 +12,5 @@ response = requests.post(
     }
 )
 
-data = response.json()    
+data = response.json()
 print(data["content"])
